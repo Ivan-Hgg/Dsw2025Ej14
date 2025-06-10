32ba359eeedd0524633944f3260d60017e94c61e
Lautaro <Lautaro.Herrera@alu.frt.utn.edu.ar>
2025-06-10 19:02:57 -0300

    hasta donde llegamos

---
+++ Dsw2025Ej14.Api/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Dsw2025Ej14.Api.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Ej14.Api.Controllers
+{
+        [ApiController]
+        [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        private readonly IPersistencia _persistencia;
+
+        public ProductsController(IPersistencia persistencia)
+        {
+            _persistencia = persistencia;
+        }
+        [HttpGet()]
+        public IActionResult GetProducts()
+        { 
+            return Ok(_persistencia.GetProducts());
+        }
+    }
+}
---
+++ Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,36 @@
+﻿using Dsw2025Ej14.Api.Domain;
+using System.Text.Json;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersistenciaEnMemoria : IPersistencia
+    {
+        private List<Product> _products = [];
+
+        public PersistenciaEnMemoria()
+        {
+            LoadProducts();
+        }
+
+        public Product GetProduct(String sku)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _products.Where(p => p.IsActive);
+        }
+
+        public void LoadProducts()
+        {
+            var json =  File.ReadAllTextAsync("products.json");
+            var products = JsonSerializer.Deserialize<List<Product>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive=true,
+            }) ??[];
+        }
+
+    }
+}
